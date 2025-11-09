3be82fef7f5817d988d90092e4c0a3dc9636b757
negretenico <negretenico0@gmail.com>
2025-11-09 16:02:16 -0500

    Adding small updates for hadnling inputss

--- App/Source/App.cpp
+++ App/Source/App.cpp
@@ -1,5 +1,15 @@
 #include "Core/Core.h"
 #include "SFML/Graphics.hpp"
+#include <iostream>
+using namespace std;
+void handleInput() {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+	{
+		// left key is pressed: move our character
+		cout << "Left key pressed" << endl;
+	}
+}
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Hello SFML");
@@ -11,8 +21,8 @@ int main()
 			if (event->is<sf::Event::Closed>())
 				window.close();
 		}
+		handleInput();
 		window.clear(sf::Color::Black);
 		window.display();
 	}
-	Core::PrintHelloWorld();
 }
